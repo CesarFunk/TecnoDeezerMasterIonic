87976fadd279f3fd2de2868b6750501974b2e2ba
DELL11253770076\DELL <juliocesar52009@live.com>
2017-07-07 11:30:42 -0600

    no message

---
+++ www/build/6.main.js
@@ -0,0 +1,45 @@
+webpackJsonp([6],{
+
+/***/ 575:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(22);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__cancion_popover__ = __webpack_require__(128);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CancionPopoverModule", function() { return CancionPopoverModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var CancionPopoverModule = (function () {
+    function CancionPopoverModule() {
+    }
+    return CancionPopoverModule;
+}());
+CancionPopoverModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__cancion_popover__["a" /* CancionPopover */],
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__cancion_popover__["a" /* CancionPopover */]),
+        ],
+        exports: [
+            __WEBPACK_IMPORTED_MODULE_2__cancion_popover__["a" /* CancionPopover */]
+        ]
+    })
+], CancionPopoverModule);
+
+//# sourceMappingURL=cancion-popover.module.js.map
+
+/***/ })
+
+});
+//# sourceMappingURL=6.main.js.map
